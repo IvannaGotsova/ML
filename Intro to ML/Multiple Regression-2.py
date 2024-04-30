2f6e9368feb5a5189103cf7d7864249cb4d24514
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-30 20:59:09 +0300

    Multiple Regression

--- Intro to ML/Multiple Regression-2.py
+++ Intro to ML/Multiple Regression-2.py
@@ -3,11 +3,8 @@ from sklearn import linear_model
 
 dataset = [
 ["Ivan", "1", "11", "11"],
-["Petar", "2", "22", "22"],
 ["Dimitar", "3", "33", "33"],
-["Stephan", "4", "44", "44"],
 ["Philip", "5", "55", "55"],
-["Robert", "6", "66", "66"],
 ["Andrei", "7", "77", "77"]
 ]
 dataframe = pd.DataFrame(dataset, columns=['Name', 'Value1', 'Value2', 'Value3'])

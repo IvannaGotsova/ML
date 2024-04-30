bdfb5336d02191b4d3dafd86a0d8603b66ae42f5
IvannaGotsova <ivannagotsova@outlook.com>
2024-04-30 18:55:54 +0300

    Percentile

--- Intro to ML/Percentile.py
+++ Intro to ML/Percentile.py
@@ -1,7 +1,10 @@
 import numpy
 
-numbers = [10, 20, 24, 28, 50, 60, 70, 80, 90, 100]
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-percentileVariable = numpy.percentile(numbers, 30)
+percentileVariable = numpy.percentile(numbers, 70)
 
 print(percentileVariable)
+
+
+

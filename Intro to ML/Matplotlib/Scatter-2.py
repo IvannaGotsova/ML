0f930092616d1dbb1d4573f5c018ea56b73ba444
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-02 19:19:27 +0300

    Scatter

---
+++ Intro to ML/Matplotlib/Scatter-2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0, 1, 3, 5, 7, 9, 10])
+y = np.array([0, 2, 4, 6, 8, 9, 10])
+x2 = np.array([1, 2, 3, 6, 9, 9, 10])
+y2 = np.array([2, 4, 4, 6, 9, 9, 10])
+x3 = np.array([0, 1, 2, 5, 6, 8, 10])
+y3 = np.array([0, 2, 5, 6, 7, 8, 10])
+
+plt.scatter(x, y)
+plt.scatter(x2, y2)
+plt.scatter(x3, y3)
+plt.show()

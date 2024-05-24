5c195a35db2f8af5d5e8e8f688d5a30ad41b51fe
IvannaGotsova <ivannagotsova@outlook.com>
2024-05-24 16:51:42 +0300

    Constants

---
+++ Intro to ML/SciPy/Constants.py
@@ -0,0 +1,13 @@
+import scipy
+
+print(scipy.constants.alpha)
+print(scipy.constants.acre)
+print(scipy.constants.bar)
+print(scipy.constants.atmosphere)
+print(scipy.constants.kilo)
+print(scipy.constants.mil)
+print(scipy.constants.atm)
+print(scipy.constants.k)
+print(scipy.constants.yard)
+print(scipy.constants.codata)
+print(scipy.constants.Btu)

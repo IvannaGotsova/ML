a84e086a7130a84c4f20c61405b1cb20ba9e81d8
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-10 21:51:36 +0300

    K Means Clustering

--- Intro to ML/Algorithms/Supervised learning/Naïve Bayes Classifier Algorithm-2.py
+++ Intro to ML/Algorithms/Supervised learning/Naïve Bayes Classifier Algorithm-2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score
@@ -58,4 +59,4 @@ disp = ConfusionMatrixDisplay(confusion_matrix=confusionMatrix, display_labels=l
 disp.plot()
 disp.show()
 
-
+plt.show()

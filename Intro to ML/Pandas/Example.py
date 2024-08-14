0ca5e9619735d0ae0d8923732a5baded6e98110d
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-14 19:50:12 +0300

    Basics

---
+++ Intro to ML/Pandas/Example.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+datasetExample = {
+  'First Name': ["Ivan", "Petar", "Dimitar", "Stoqn", "Philip", "Stephan"],
+  'Last Name': ["Ivanov", "Petrov", "Dimitrov", "Stoqnov", "Philipov", "Stephanov"],
+  'Age': [39, 43, 47, 42, 39, 36],
+  'City': ["Sofia", "Sofia", "Plovdiv", "Sofia", "Sofia", "Varna"],
+  'Salary': [1000, 1100, 1000, 1000, 1100, 1000],
+  'Experience': [10, 15, 17, 14, 11, 5],
+  'Grades': [4, 5, 6, 5, 4, 4]
+}
+
+dataframeExample = pd.DataFrame(datasetExample)
+
+print(dataframeExample)

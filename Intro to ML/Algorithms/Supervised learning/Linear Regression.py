119f5fd0ae24fef497b5082fe8df6f1c9f23a144
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-18 11:59:44 +0300

    Creating Model

--- Intro to ML/Algorithms/Supervised learning/Linear Regression.py
+++ Intro to ML/Algorithms/Supervised learning/Linear Regression.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.linear_model import LinearRegression
 
 datasetExample = {
     'First Name': ["Ivan", "Petar", "Dimitar", "Stoqn", "Philip", "Stephan"],
@@ -20,7 +22,9 @@ print(dataframeExample)
 
 plt.scatter(dataframeExample['Age'], dataframeExample['Salary'])
 plt.scatter(dataframeExample['Grades'], dataframeExample['Experience'])
+#plt.show()
 
-plt.show()
 
+linearRegression = LinearRegression()
 
+linearRegression.fit(dataframeExample[['Age']], dataframeExample[['Salary']])

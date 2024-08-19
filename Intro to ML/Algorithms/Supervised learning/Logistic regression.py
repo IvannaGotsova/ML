94184dc12fe3458d1373f1c6439e75343253e223
IvannaGotsova <ivannagotsova@outlook.com>
2024-08-19 19:09:45 +0300

    Creating training and test set

--- Intro to ML/Algorithms/Supervised learning/Logistic regression.py
+++ Intro to ML/Algorithms/Supervised learning/Logistic regression.py
@@ -16,3 +16,13 @@ plt.title("Age / Salary")
 plt.plot(Age, Output, color="b")
 #plt.show()
 
+X_train, X_test, y_train, y_test = train_test_split(Age, Output, test_size=0.2, random_state=42)
+
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
+print()
+
+
+

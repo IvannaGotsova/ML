c438852526cc4d109f3920fd99b8b6175afbba5c
IvannaGotsova <ivannagotsova@outlook.com>
2024-10-13 15:37:59 +0300

    Added data

--- Intro to ML/Algorithms/Unsupervised Learning/Apriori algorithm.py
+++ Intro to ML/Algorithms/Unsupervised Learning/Apriori algorithm.py
@@ -2,19 +2,25 @@ import numpy as np
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
 
-dataset = [
-["Ivan", "Plovdiv", "Mathematics", "22", "6", "91.2"],
-["Petar", "Sofia", "Biology","35", "5", "63.5"],
-["Dimitar", "Varna", "Mathematics","43", "5", "90.23"],
-["Stephan", "Burgas","Computer Science", "45", "4", "92.7"],
-["Philip", "Sofia", "Statistics","49", "6", "98.2"],
-["Robert", "Sofia", "Mathematics","23", "3", "88.1"],
-["Andrei", "Plovdiv", "Biology","34", "4", "95.0"]
+data = [
+    ['1', ['Bread', 'Milk', 'Vegetables', 'Salad']],
+    ['2', ['Milk', 'Fruits', 'Cheese', 'Fruits', 'Eggs', 'Juice']],
+    ['3', ['Milk', 'Salad', 'Cheese']],
+    ['4', ['Bread', 'Milk', 'Fruits', 'Eggs', 'Cheese', 'Vegetables']],
+    ['5', ['Bread', 'Salad', 'Eggs', 'Milk', 'Salad', 'Vegetables']],
+    ['6', ['Milk', 'Salad', 'Bread', 'Cheese', 'Vegetables', 'Eggs', 'Ice Cream']],
+    ['7', ['Bread', 'Salad', 'Eggs', 'Milk']],
+    ['8', ['Bread', 'Milk', 'Salad', 'Vegetables']],
+    ['9', ['Bread', 'Milk', 'Salad', 'Vegetables']],
+    ['10', ['Bread', 'Milk', 'Vegetables', 'Salad', 'Eggs']],
+    ['11', ['Milk', 'Fruits', 'Salad', 'Eggs']],
+    ['12', ['Milk', 'Salad', 'Cheese', 'Vegetables', 'Eggs', 'Milk']],
+    ['13', ['Bread', 'Milk', 'Fruits']],
+    ['14', ['Bread', 'Salad', 'Ice Cream', 'Milk', 'Vegetables']],
+    ['15', ['Milk', 'Salad', 'Fruits', 'Cheese', 'Fruits']],
+    ['16', ['Bread', 'Salad', 'Fruits', 'Milk', 'Juice']],
+    ['17', ['Bread', 'Milk', 'Salad', 'Vegetables', 'Eggs', 'Cheese', 'Vegetables']],
+    ['18', ['Bread', 'Milk', 'Salad', 'Eggs', 'Cheese']],
+    ['Я9', ['Bread', 'Milk', 'Salad', 'Vegetables']],
+    ['20', ['Bread', 'Milk', 'Salad', 'Vegetables']]
 ]
-
-dataframe = pd.DataFrame(dataset, columns=['Name', "City", 'Subject', 'Age', 'Grade', 'Percentage'])
-
-
-
-
-

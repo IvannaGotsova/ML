b9743ad946000a63b46723875075b77d07482365
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-09 21:44:46 +0200

    XGBoost - added data

--- Intro to ML/Algorithms/Supervised learning/XGBoost.py
+++ Intro to ML/Algorithms/Supervised learning/XGBoost.py
@@ -6,17 +6,22 @@ from xgboost import XGBClassifier
 pd.set_option('future.no_silent_downcasting', True)
 
 datasetExample = {
-    'Day': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    'Day': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
     'Outlook': ["Sunny", "Overcast", "Rainy", "Rainy", "Overcast", "Sunny", "Rainy", "Sunny", "Overcast", "Rainy",
-                "Overcast", "Sunny", "Rainy", "Sunny", "Overcast", "Rainy", "Sunny", "Rainy", "Sunny", "Overcast"],
+                "Overcast", "Sunny", "Rainy", "Sunny", "Overcast", "Rainy", "Sunny", "Rainy", "Sunny", "Overcast",
+                "Sunny", "Rainy", "Sunny", "Overcast", "Rainy", "Overcast", "Sunny", "Rainy", "Sunny", "Overcast"],
     'Temperature': ["Hot", "Hot", "Cold", "Cold", "Mild", "Mild", "Cold", "Hot", "Mild", "Cold",
+                    "Hot", "Cold", "Cold", "Mild", "Mild", "Hot", "Cold", "Cold", "Mild", "Mild",
                     "Hot", "Cold", "Cold", "Mild", "Mild", "Hot", "Cold", "Cold", "Mild", "Mild"],
     'Humidity': ["High", "High", "Normal", "Normal", "High", "High", "Normal", "High", "High", "Normal",
+                 "High", "High", "Normal", "Normal", "High", "High", "Normal", "High", "High", "Normal",
                  "Normal", "Normal", "High", "Normal", "Normal", "High", "Normal", "Normal", "High", "Normal"],
     'Wind': ["No", "Yes", "Yes", "Yes", "No", "No", "No", "Yes", "No", "No",
+             "No", "Yes", "No", "No", "Yes", "Yes", "Yes", "No", "No", "Yes",
              "Yes", "Yes", "Yes", "No", "No", "Yes", "Yes", "Yes", "No", "No"],
     'Predicted': ["No", "Yes", "No", "No", "Yes", "Yes", "Yes", "No", "Yes", "No",
-                  "Yes", "Yes", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "No", "Yes"]
+                  "Yes", "Yes", "No", "Yes", "No", "Yes", "Yes", "Yes", "No", "Yes",
+                  "Yes", "Yes", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "No", "No"]
 }
 
 dataframeExample = pd.DataFrame(datasetExample)

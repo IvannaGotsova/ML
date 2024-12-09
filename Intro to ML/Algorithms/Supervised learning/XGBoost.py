fb998c83f52bb73d667df55ed812535f595b18c6
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-09 21:46:19 +0200

    XGBoost - change test_size

--- Intro to ML/Algorithms/Supervised learning/XGBoost.py
+++ Intro to ML/Algorithms/Supervised learning/XGBoost.py
@@ -45,7 +45,7 @@ X = dataframeExample[['Day', 'Outlook', 'Temperature', 'Humidity', 'Wind']]
 Y = dataframeExample["Predicted"]
 
 
-test_size = 0.2
+test_size = 0.3
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)
 
 

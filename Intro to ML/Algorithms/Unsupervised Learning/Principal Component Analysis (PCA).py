fa43956710e785336caa97fc777cac390630546b
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-12 20:48:18 +0200

    Principal Component Analysis (PCA) - train

--- Intro to ML/Algorithms/Unsupervised Learning/Principal Component Analysis (PCA).py
+++ Intro to ML/Algorithms/Unsupervised Learning/Principal Component Analysis (PCA).py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 datasetExample = {
@@ -68,3 +69,10 @@ sc = StandardScaler()
 
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+
+pca = PCA(n_components=6)
+
+X_train = pca.fit_transform(X_train)
+X_test = pca.transform(X_test)
+
+explained_variance = pca.explained_variance_ratio_

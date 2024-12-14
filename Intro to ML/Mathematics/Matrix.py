cf1213c0a9d0581487aff3ce4069fbf82cd110ba
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-14 17:35:34 +0200

    Matrix

---
+++ Intro to ML/Mathematics/Matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+matrix_one = [ [1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9] ]
+
+print(matrix_one)
+
+matrix_two = np.array([ [1, 2, 3],
+                        [4, 5, 6],
+                        [7, 8, 9] ])
+print(matrix_two)

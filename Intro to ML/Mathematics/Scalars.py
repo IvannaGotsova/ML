b133264192a1a951fdbe0081f1f924991b38370e
IvannaGotsova <ivannagotsova@outlook.com>
2024-12-14 17:30:58 +0200

    Scalars

---
+++ Intro to ML/Mathematics/Scalars.py
@@ -0,0 +1,17 @@
+scalar_number = 35
+print(scalar_number)
+
+scalar_number = 53
+print(scalar_number)
+
+scalar_number = 90
+print(scalar_number)
+
+scalar_number = 19
+print(scalar_number)
+
+scalar_number = 65
+print(scalar_number)
+
+scalar_number = 43
+print(scalar_number)

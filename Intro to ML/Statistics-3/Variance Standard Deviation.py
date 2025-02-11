bdd08a92baa601ba5222576856000b72d0bcc52f
IvannaGotsova <ivannagotsova@outlook.com>
2025-02-11 19:59:08 +0200

    Variance Standard Deviation

---
+++ Intro to ML/Statistics-3/Variance Standard Deviation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+numbers = [19, 23, 34, 35, 38, 55, 61, 79, 99, 100]
+
+numbersVariance = np.var(numbers)
+
+print(numbersVariance)
+
+numbers = [19, 23, 34, 35, 38, 55, 61, 79, 99, 100]
+
+standardDeviationVariable = np.std(numbers)
+
+print(standardDeviationVariable)

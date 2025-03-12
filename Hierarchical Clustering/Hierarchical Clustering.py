287cd66d2a8a4f7f009ed772d7771d485810f85d
IvannaGotsova <ivannagotsova@outlook.com>
2025-03-12 13:49:00 +0200

    Clustering - Hierarchical clustering

---
+++ Hierarchical Clustering/Hierarchical Clustering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans, AgglomerativeClustering
+
+dataset = [
+["Ivan", "Plovdiv", "Mathematics", "22", "6", "91.2", 1],
+["Petar", "Sofia", "Biology","35", "5", "63.5", 0],
+["Dimitar", "Varna", "Mathematics","43", "5", "90.23", 0],
+["Stephan", "Burgas","Computer Science", "45", "4", "92.7", 1],
+["Philip", "Sofia", "Statistics","49", "6", "98.2", 1],
+["Robert", "Sofia", "Mathematics","23", "3", "88.1", 1],
+["Andrei", "Plovdiv", "Biology","34", "4", "95.0", 0]
+]
+
+dataframe = pd.DataFrame(dataset, columns=['Name', "City", 'Subject', 'Age', 'Grade', 'Percentage', "Score"])
+
+
+grade = dataframe["Grade"].astype(int)
+percentage = dataframe["Percentage"].astype(float)
+
+data = list(zip(grade, percentage))
+
+
+clustering = AgglomerativeClustering(n_clusters=2).fit(data)

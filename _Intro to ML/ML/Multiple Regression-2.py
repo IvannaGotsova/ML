5fe2a0c2b324c025697e0a2a28a31e2bc10fb690
IvannaGotsova <ivannagotsova@outlook.com>
2025-06-26 13:52:05 +0300

    Multiple regression

---
+++ _Intro to ML/ML/Multiple Regression-2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn import linear_model
+
+dataset = [
+["Ivan", "1", "11", "11"],
+["Dimitar", "3", "33", "33"],
+["Philip", "5", "55", "55"],
+["Andrei", "7", "77", "77"]
+]
+dataframe = pd.DataFrame(dataset, columns=['Name', 'Value1', 'Value2', 'Value3'])
+
+X = dataframe[['Value1', 'Value2']]
+y = dataframe['Value3']
+
+linearregression = linear_model.LinearRegression()
+linearregression.fit(X.values, y.values)
+
+predictedvalue= linearregression.predict([[100, 1000]])
+
+print(predictedvalue)

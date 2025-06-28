3c4a7ca1d42f528c3e8f35610cd6154098ec3856
IvannaGotsova <ivannagotsova@outlook.com>
2025-06-28 13:38:22 +0300

    Train testectors

---
+++ _Intro to ML/Mathematics/Vectors.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+vector_one = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(vector_one)
+
+vector_two = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(vector_two)
+
+vector_three = pd.Series([1, 2, 3, 4, 5])
+print(vector_three)
